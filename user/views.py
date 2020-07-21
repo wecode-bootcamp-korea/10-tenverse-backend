4e5856d1c2e9b8724b7a12946938e5b1224349cd
soochung sohn <soochungsohn@MacBook-Pro.local>
2020-07-22 08:34:43 +0900

    Modify : validation 수정     - validation 함수 추가

--- user/views.py
+++ user/views.py
@@ -13,25 +13,43 @@ from .models           import (
     Gender,
 )
 
+def validation(email, password):
+    if len(password) < 8:
+        return False
+    email_lambdas = [
+        lambda x : '@' in x,
+        lambda x : '.' in x
+    ]
+    password_lambdas = [
+        lambda x : x in ['!', '@', '#', '$', '%', '&', '*'],
+        lambda x : x in ['0','1','2','3','4','5','6','7','8','9']
+    ]
+    for lambdas in email_lambdas:
+        if not lambdas(email):
+            return False
+    for lambdas in password_lambdas:
+        if not any(lambdas(i) for i in password):
+            return False
+    return True
+
 class SignUpView(View):
     def post(self, request):
         data = json.loads(request.body)
         try:
-            if re.search(re.compile("[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-zA-Z]+$"), data['email']):
-                if re.search(re.compile("^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%&*])[a-zA-Z\d!@#$%&*]{8,}$"), data['password']):
-                    if User.objects.filter(email=data['email']).exists():
-                        return JsonResponse({'message' : 'EXISTING_ACCOUNT'}, status=400)
-                    hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
-                    User(
-                        name         = data['name'],
-                        phone_number = data['phone_number'],
-                        birth_date   = data['birth_date'],
-                        gender_id    = Gender.objects.get(name = data['gender']).id,
-                        email        = data['email'],
-                        password     = hashed_password.decode('utf-8'),
-                    ).save()
-                    return JsonResponse({'message' : 'SUCCESS'}, status = 200)
-            return JsonResponse({'message' : 'VALIDATION_ERROR'}, status=401)
+            if not validation(data['email'], data['password']):
+                return JsonResponse({'message' : 'VALIDATION_ERROR'}, status=401)
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message' : 'EXISTING_ACCOUNT'}, status=400)
+            hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+            User(
+                name         = data['name'],
+                phone_number = data['phone_number'],
+                birth_date   = data['birth_date'],
+                gender_id    = Gender.objects.get(name = data['gender']).id,
+                email        = data['email'],
+                password     = hashed_password.decode('utf-8'),
+            ).save()
+            return JsonResponse({'message' : 'SUCCESS'}, status = 200)
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
 
@@ -39,11 +57,13 @@ class SignInView(View):
     def post(self, request):
         data = json.loads(request.body)
         try:
+            if not validation(data['email'], data['password']):
+                return JsonResponse({'message' : 'VALIDATION_ERROR'}, status=401)
             if User.objects.filter(email = data['email']).exists():
                 user = User.objects.get(email = data['email'])
                 if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
-                   access_token =  jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM)
-                   return JsonResponse({'access_token' : access_token.decode('utf-8')}, status = 200)
+                    access_token =  jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM)
+                    return JsonResponse({'access_token' : access_token.decode('utf-8')}, status = 200)
                 return JsonResponse({'message' : 'UNAUTHORIZED'}, status = 401)
             return JsonResponse({'message' : 'UNAUTHORIZED'}, status = 401)
         except KeyError:

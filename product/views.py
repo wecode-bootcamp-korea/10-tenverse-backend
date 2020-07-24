3cab036d96a97fa68c4d43b7049f199accf19cd2
soochung sohn <soochungsohn@MacBook-Pro.local>
2020-07-24 23:01:00 +0900

    Modify : 코드 정렬 수정

--- product/views.py
+++ product/views.py
@@ -25,13 +25,13 @@ class MainPageView(View):
     def get(self, request):
         shoes = list(ShoeColor.objects.filter(**{
             'shoe__detail__is_main' : 'True',
-            'color__name__in' : ['화이트', '노마드카키'],
-            'subimage__is_hover' : 'True'
+            'color__name__in'       : ['화이트', '노마드카키'],
+            'subimage__is_hover'    : 'True'
         }).values('id', 'color_id__name','shoe_id__price','image__image', 'subimage__image'))
         shoes.append(list(ShoeColor.objects.filter(**{
             'shoe_id__detail__name' : '척테일러 올스타 리프트 캔버스',
-            'color_id__name' : '블랙',
-            'subimage__is_hover' : 'True'
+            'color_id__name'        : '블랙',
+            'subimage__is_hover'    : 'True'
         }).values('id', 'color_id__name','shoe_id__price','image__image', 'subimage__image'))[0])
         return JsonResponse({'product' : shoes}, status=200)
 

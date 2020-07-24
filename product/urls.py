ebb94d730a39611765530a544bdf6736c706c98a
soochung sohn <soochungsohn@MacBook-Pro.local>
2020-07-24 15:40:48 +0900

    Add : Mainview, filter_category view 엔드포인트 추가     - Mainview limit 추가 중

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import ShoesView
+from .views import (
+    ShoesView,
+    CategoryView
+)
 
 urlpatterns = [
     path('', ShoesView.as_view()),
+    path('/category', CategoryView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,62 @@
+import time
+
 from django.views   import View
 from django.http    import JsonResponse
 
+from .models import (
+    MainCategory,
+    ShoeCategory,
+    Shoe,
+    Size,
+    ColorFilter,
+    Color,
+    TypeFilter,
+    GenderSegmentation,
+    Detail,
+    ShoeSize,
+    ShoeColor,
+    SubImage
+)
+class CategoryView(View):
+    def get(self, request):
+        filter_list = {
+            'gender_filters' : [gender.name for gender in GenderSegmentation.objects.all()],
+            'color_filters'  : [color.name for color in ColorFilter.objects.all()],
+            'type-filters'   : [typefilter.name for typefilter in TypeFilter.objects.all()],
+            'size_filters'   : [size.name for size in Size.objects.all()]
+        }
+        return JsonResponse({'filters' : filter_list}, status=200)
+
 class ShoesView(View):
     def get(self, request):
-        return JsonResponse({'message' : 'test'}, status=200)
+        shoe_list   = []
+        shoes       = Shoe.objects.all().prefetch_related('color').select_related('detail')
+        for shoe in shoes:
+            name             = Detail.objects.get(id = shoe.detail_id).name
+            price            = shoe.price
+            colorfilter_list = []
+            shoecolors       = ShoeColor.objects.filter(shoe_id = shoe.id).select_related('image').prefetch_related('subimage_set')
+            
+            for colors in shoecolors:
+                image        = colors.image.image
+                sub_image    = SubImage.objects.filter(shoe_color = colors).first().image
+                colorfilters = Color.objects.select_related('color_category').filter(id = colors.color_id)
+
+                for color in colorfilters:
+                    colorfilter_list.append({
+                        'id'    : color.id,
+                        'color' : ColorFilter.objects.get(id=color.color_category.id).name
+                    })
+                
+                shoe_list.append({
+                    'name'        : name,
+                    'price'       : price,
+                    'image'       : image,
+                    'hover_image' : sub_image,
+                    'colors'      : colorfilter_list
+                })
+                
+            for shoe in shoe_list:
+                if shoe['name'] == name:
+                    shoe['colors'] = colorfilter_list
+        return JsonResponse({'products' : shoe_list}, status=200)

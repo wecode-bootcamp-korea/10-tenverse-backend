2e2a4107e4631196cf2ca48c5299242c1242398a
soochung sohn <soochungsohn@MacBook-Pro.local>
2020-07-27 19:42:39 +0900

    Modify : 리뷰사항 반영

--- product/views.py
+++ product/views.py
@@ -31,7 +31,6 @@ class CategoryView(View):
 
 class MainPageView(View):
     def get(self, request):
-        shoes = {}
         shoe = ShoeColor.objects.filter(**{
             'shoe__detail__is_main' : True,
             'subimage__is_hovered'  : True
@@ -42,14 +41,14 @@ class MainPageView(View):
             sub_image  = F('subimage__image')
         ).values('id','name','price','main_image','sub_image')
         
-        shoes['womens_collection'] = list(shoe.filter(shoe__detail__name__contains = '척테일러'))
-        
-        shoes['jack_purcell'] = list(shoe.filter(color__name = '노마드카키'))
-        
-        shoes['pro_leather'] = list(shoe.filter(**{
-            'shoe__detail__name__contains' : '잭퍼셀',
-            'color__name'                  : '화이트'
-        }))
+        shoes = {
+            'womens_collection' : list(shoe.filter(shoe__detail__name__contains = '척테일러')),
+            'jack_purcell'      : list(shoe.filter(color__name = '노마드카키')),
+            'pro_leather'       : list(shoe.filter(**{
+                'shoe__detail__name__contains' : '잭퍼셀',
+                'color__name'                  : '화이트'
+            }))
+        }
 
         return JsonResponse({'products' : shoes}, status=200)
 
